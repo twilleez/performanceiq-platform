4ef35cf8cf439972d825ab7667f774ca6012d615
twilleez <wjhill@gmail.com>
2026-02-05 13:16:31 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // config.js
-window.PERFIQ = {
-  SUPABASE_URL: "YOUR_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "YOUR_SUPABASE_ANON_KEY"
-};
+window.PERFIQ = window.PERFIQ || {};
+window.PERFIQ.SUPABASE_URL = "https://YOURPROJECT.supabase.co";
+window.PERFIQ.SUPABASE_ANON_KEY = "YOUR_ANON_PUBLIC_KEY";
