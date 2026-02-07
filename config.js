70554d1b11770ceae17cc5297413fcab1aead855
twilleez <wjhill@gmail.com>
2026-02-06 23:11:43 -0500

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
   "use strict";
 
   window.SUPABASE_URL = "https://znhxaqlvkpcskvzljiev.supabase.co";
-  window.SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE_AS_A_STRING";
+  window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuaHhhcWx2a3Bjc2t2emxqaWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNjM5MzksImV4cCI6MjA4NTczOTkzOX0.ReYzuZuGEI6aqcrXc_jIAXnamtgwCILMwcTsVYAtII0";
 
   window.PIQ_VERSION = "1.0.5";
 
