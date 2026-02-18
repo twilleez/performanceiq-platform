3573343fefa5a0c819da152c97a733629b24c500
twilleez <wjhill@gmail.com>
2026-02-18 11:27:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -35,8 +35,8 @@
   // Optional: Supabase keys can live here too (see supabaseClient.js below)
   window.PIQ_CONFIG.supabase = Object.assign(
     {
-      url: "",
-      anonKey: ""
+      url: "https://znhxaqlvkpcskvzljiev.supabase.co",
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpuaHhhcWx2a3Bjc2t2emxqaWV2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxNjM5MzksImV4cCI6MjA4NTczOTkzOX0.ReYzuZuGEI6aqcrXc_jIAXnamtgwCILMwcTsVYAtII0"
     },
     window.PIQ_CONFIG.supabase || {}
   );
