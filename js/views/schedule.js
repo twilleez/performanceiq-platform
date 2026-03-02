c17608762710c396110f4a0dcde98dc12610472b
twilleez <wjhill@gmail.com>
2026-03-01 21:57:01 -0500

    Update schedule.js

--- js/views/schedule.js
+++ js/views/schedule.js
@@ -1,1 +1,14 @@
+import { EVENTS } from '../data/demo.js';
 
+export function renderSchedule(mountEl){
+  if (!mountEl) return;
+  mountEl.innerHTML = EVENTS.map(ev => `
+    <div class="event-item">
+      <div style="text-align:center;min-width:36px">
+        <div class="event-days-num ${ev.days<=3?'soon':''}">${ev.days}</div>
+        <div class="event-days-label">days</div>
+      </div>
+      <div><div class="event-name">${ev.name}</div><div class="event-detail">${ev.detail}</div></div>
+      <div style="font-size:17px;margin-left:auto">${ev.icon}</div>
+    </div>`).join('');
+}
