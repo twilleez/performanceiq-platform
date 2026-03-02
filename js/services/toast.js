fccedf85d5ffff2d10a79a1be2c01e4806aa03e1
twilleez <wjhill@gmail.com>
2026-03-01 21:45:52 -0500

    Update toast.js

--- js/services/toast.js
+++ js/services/toast.js
@@ -1,1 +1,22 @@
+import { dom } from '../ui/dom.js';
 
+export function toast(msg, ms = 2800) {
+  const host = dom.toastContainer || (() => {
+    const div = document.createElement('div');
+    div.id = 'toastContainer';
+    document.body.appendChild(div);
+    return div;
+  })();
+
+  const t = document.createElement('div');
+  t.className = 'toast';
+  t.textContent = msg;
+  host.appendChild(t);
+
+  setTimeout(() => {
+    t.style.transition = 'opacity .25s,transform .25s';
+    t.style.opacity = '0';
+    t.style.transform = 'translateY(6px)';
+    setTimeout(() => t.remove(), 280);
+  }, ms);
+}
