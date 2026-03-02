db5e1469e6f558650bd6b89180fb3b760b16bcf0
twilleez <wjhill@gmail.com>
2026-03-01 20:36:02 -0500

    Update keys.js

--- js/state/keys.js
+++ js/state/keys.js
@@ -1,1 +1,5 @@
-
+export const STORAGE_KEY_ONBOARDED = 'piq_onboarded_v2';
+export const STORAGE_KEY_STATE     = 'piq_state_v2';
+export const STORAGE_KEY_ATHLETES  = 'piq_athletes_v2';
+export const STORAGE_KEY_TOUR      = 'piq_tour_today_v1';
+export const THEME_KEY             = 'piq_theme';
