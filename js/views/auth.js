a540a711c4aa43a8d052b16f119ba839e829b67d
twilleez <wjhill@gmail.com>
2026-03-09 15:09:33 -0400

    Add files via upload

---
+++ js/views/auth.js
@@ -0,0 +1,12 @@
+export function renderAuth(){
+return `
+<div style="max-width:400px;margin:100px auto">
+<div class="card">
+<h2>Login</h2>
+<input id="email" placeholder="email">
+<br><br>
+<button id="login-btn">Login</button>
+</div>
+</div>
+`
+}
