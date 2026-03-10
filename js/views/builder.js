77f067c40b11a09d426f579dca5f5647e7263e3a
twilleez <wjhill@gmail.com>
2026-03-10 14:55:19 -0400

    Create builder.js

---
+++ js/views/builder.js
@@ -0,0 +1,17 @@
+export function builderView(){
+
+return `
+
+<div class="card">
+
+<h2>Workout Builder</h2>
+
+<input placeholder="Workout name">
+
+<button>Create Workout</button>
+
+</div>
+
+`
+
+}
