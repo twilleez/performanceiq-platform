f5eddd7ccfe7737692bbe0251f0b68c5d00b0ede
twilleez <wjhill@gmail.com>
2026-03-10 14:56:56 -0400

    Add periodization function to render HTML structure

---
+++ js/views/periodization.js
@@ -0,0 +1,22 @@
+export function periodization(){
+
+return `
+
+<div class="card">
+
+<h2>12 Week Periodization</h2>
+
+<ul>
+
+<li>Weeks 1-4 foundation</li>
+<li>Weeks 5-8 strength</li>
+<li>Weeks 9-11 power</li>
+<li>Week 12 deload</li>
+
+</ul>
+
+</div>
+
+`
+
+}
