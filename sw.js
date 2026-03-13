8ad338292672919356e23ddf5a9f4e256d729673
twilleez <wjhill@gmail.com>
2026-03-13 10:33:39 -0400

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE='piq-v2';
-const ASSETS=['./','./index.html','./styles.css','./core.js','./dataStore.js','./exerciseLibrary.js'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match('./index.html')))));
